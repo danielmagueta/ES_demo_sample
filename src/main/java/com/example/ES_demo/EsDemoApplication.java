a42252967bd2cb454a53c45969e95cd92bd943d3
Daniel Magueta <daniel.magueta@hotmail.com>
2021-05-17 15:19:51 +0100

    second commit

--- src/main/java/com/example/ES_demo/EsDemoApplication.java
+++ src/main/java/com/example/ES_demo/EsDemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EsDemoApplication {
 
 	public static void main(String[] args) {
+                
+                System.out.println("Hello World");
 		SpringApplication.run(EsDemoApplication.class, args);
 	}
 
