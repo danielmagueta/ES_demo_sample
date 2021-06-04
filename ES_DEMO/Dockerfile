ffae00358babf9bbfebca842d11f642001f1eb62
Daniel Magueta <daniel.magueta@hotmail.com>
2021-06-04 12:16:02 +0100

    changes in structure

---
+++ ES_DEMO/Dockerfile
@@ -0,0 +1,4 @@
+FROM adoptopenjdk/openjdk11:latest
+EXPOSE 8080
+ADD target/ES_demo-0.0.1-SNAPSHOT ES_demo-0.0.1-SNAPSHOT
+ENTRYPOINT ["java","-jar","/ES_demo-0.0.1-SNAPSHOT"]
---
+++ ES_DEMO/src/test/java/com/example/ES_demo/EsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ES_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
